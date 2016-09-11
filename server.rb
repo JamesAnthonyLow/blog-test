fb6eae3120becec2140a851c6be5b0e19d9f4150
JamesAnthonyLow <jamesalowenthal@gmail.com>
2016-09-11 19:34:09 -0400

    rename public views

--- server.rb
+++ server.rb
@@ -1,15 +1,5 @@
 require 'sinatra'
 
-set :public_folder, 'public'
-
 get '/' do
-  redirect '/index.html'
-end
-
-get '/archive' do
-  e_dir = "public/entries/"
-  entries = Dir[e_dir+"*.html"].map do |e| 
-    File.basename(e, ".html").gsub("_", " ")
-  end
-  entries.join("\n")
+  erb :index
 end
