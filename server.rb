87b64b9fb3782206015fe621b0c997eddaade321
JamesAnthonyLow <jamesalowenthal@gmail.com>
2016-09-11 18:06:37 -0400

    start working on JSON server for archive

--- server.rb
+++ server.rb
@@ -5,3 +5,11 @@ set :public_folder, 'public'
 get '/' do
   redirect '/index.html'
 end
+
+get '/archive' do
+  e_dir = "public/entries/"
+  entries = Dir[e_dir+"*.html"].map do |e| 
+    File.basename(e, ".html").gsub("_", " ")
+  end
+  entries.join("\n")
+end
