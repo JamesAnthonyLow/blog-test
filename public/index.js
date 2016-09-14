3342c39465922e47eea86e3320ac2f9a79636c80
JamesAnthonyLow <jamesalowenthal@gmail.com>
2016-09-13 21:08:19 -0400

    line numbers and titles

--- public/index.js
+++ public/index.js
@@ -1,18 +1,54 @@
-var select = function(str){ return document.getElementById(str); }
+var select = function( str ){ return document.getElementById( str ); }
+
+var fontHeight = function(){
+  var div = document.createElement( "div" );
+  div.className = "code-font";
+  div.innerHTML += "0";
+  var body = document.getElementsByTagName( "body" )[0];
+  body.appendChild( div );
+  offset = div.offsetHeight;
+  body.removeChild( div );
+  return offset;
+}
+
+var getBarHeight = function(){
+  return select( "code-body" ).offsetHeight / fontHeight();
+}
 
 var addLineNumbers = function(){
-  var nBar = select("numbers")
+  for(var i=0; i<getBarHeight(); i++)
+  select( "numbers" ).innerHTML += 
+    "<div id=\"box-"+i+"\" class=\"num\">"+i+"</div>";
+}
 
-  nBar.innerHTML += '<div id="first-num">0</div>'
-
-  var barHeight = (nBar.offsetHeight / select("first-num").offsetHeight)
-
-  for(var i=1; i<barHeight; i++){
-    nBar.innerHTML += "<div>" + i + "</div>"
+var colorNumber = function( ev ){
+  var num = Math.floor( ev.clientY/fontHeight() );
+  var all_nums = document.getElementsByClassName( "num" );
+  if( num <= getBarHeight() ){
+    for(var i=0; i<all_nums.length; i++){
+      all_nums[i].innerHTML = Math.abs( i-num );
+    }
+    var line = select( "box-"+String( num ) );
   }
 }
 
-document.addEventListener("DOMContentLoaded", function(event){
+var toggleTitle = function( board, titleSpan ){
+  var board = select( board );
+  var titleSpan = select( titleSpan );
+  board.onmouseenter = function(){
+    titleSpan.classList.remove( "hidden" );
+  }
+  board.onmouseleave = function(){
+    titleSpan.classList.add( "hidden" );
+  }
+}
+
+document.addEventListener( "DOMContentLoaded", function( event ){
+  document.onmousemove = colorNumber; 
+
+  toggleTitle( "posts", "post-title" );
+  toggleTitle( "social", "social-title" );
+
   addLineNumbers();
 });
 
